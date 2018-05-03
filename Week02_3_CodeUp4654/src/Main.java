b2eb8463bcad854909070620d26f58a798e25502
kevin0309 <32089450+kevin0309@users.noreply.github.com>
2018-05-03 16:21:57 +0900

    단일 for문으로 어떻게할지..

--- Week02_3_CodeUp4654/src/Main.java
+++ Week02_3_CodeUp4654/src/Main.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
 
@@ -11,33 +8,60 @@ public class Main {
 		int numberOfTower = scan.nextInt();
 		Scanner scan2 = new Scanner(System.in);
 		String towerHeightListPlainStr = scan2.nextLine().trim();
-		/*int numberOfTower = 5;
-		String towerHeightListPlainStr = "6 9 5 7 4";*/
+		//String towerHeightListPlainStr = "1 2 5 7 4 4 1 2 5 12 12 12 13";
 		
-		StringTokenizer towerHeightListStr = new StringTokenizer(towerHeightListPlainStr," ");
-		long[] towerHeightList = new long[numberOfTower];
-		for (int i = 0; i < numberOfTower; i++) {
-			try {
-				int newint = Integer.parseInt(towerHeightListStr.nextToken());
-				towerHeightList[i] = newint;
-			} catch (Exception e) {
-				throw new RuntimeException("i : "+i+", length : "+towerHeightListStr.countTokens());
+		/*StringTokenizer st = new StringTokenizer(towerHeightListPlainStr, " ");
+		int len = st.countTokens();
+		System.out.println("len : " + len);
+		int[] towerHeightList = new int[len];
+		for (int i = 0; i < len; i++)
+			towerHeightList[i] = Integer.parseInt(st.nextToken());
+
+		String result = "0 ";
+		int max = towerHeightList[0];
+		int maxIndex = 0;
+		for (int i = 1; i < len; i++) {
+			if (max == towerHeightList[i]) {
+				result += "0 ";
+				max = towerHeightList[i];
+				maxIndex = i+1;
+			}
+			else if (max <= towerHeightList[i]) {
+				result += maxIndex+" ";
+				max = towerHeightList[i];
+				maxIndex = i+1;
+			}
+			else if (towerHeightList[i] <= towerHeightList[i-1]) {
+				max = towerHeightList[i-1];
+				maxIndex = i;
+				result += maxIndex+" ";
+			}
+			else {
+				result += maxIndex+" ";
 			}
 		}
 		
-		int[] result = new int[numberOfTower];
-		for (int i = 1; i < numberOfTower; i++) {
+		System.out.println(result);*/
+		
+		String[] st = towerHeightListPlainStr.split(" "); 
+		//StringTokenizer st = new StringTokenizer(towerHeightListPlainStr," ");
+		int len = st.length;
+		int[] towerHeightList = new int[len];
+		towerHeightList[0] = Integer.parseInt(st[0]);
+		String result = "";
+		for (int i = 1; i < len; i++) {
+			towerHeightList[i] = Integer.parseInt(st[i]);
 			for (int j = i-1; j > -1; j--) {
 				if (towerHeightList[i] <= towerHeightList[j]) {
-					result[i] = j+1;
+					result += j+1+" ";
 					break;
 				}
+				if (j == 0)
+					result += "0 ";
 			}
 		}
-		
-		for (int res : result) {
-			System.out.print(res+" ");
-		}
+		//0 0 0 4 5 6 6 4 0 10 11 0 
+		System.out.println(result);
 	}
 
 }
